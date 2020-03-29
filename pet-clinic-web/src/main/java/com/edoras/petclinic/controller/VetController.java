ced73e0b8f3f6ba3103944748cb6e5eb37da5f09
emrecalik <emre.calik01@gmail.com>
2020-03-29 14:50:09 +0300

    loading list of vets page. #23

--- pet-clinic-web/src/main/java/com/edoras/petclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/edoras/petclinic/controller/VetController.java
@@ -1,14 +1,22 @@
 package com.edoras.petclinic.controller;
 
+import com.edoras.petclinic.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/vet")
 public class VetController {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
 
     @RequestMapping({"", "/", "index", "index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vet/index";
     }
 }
