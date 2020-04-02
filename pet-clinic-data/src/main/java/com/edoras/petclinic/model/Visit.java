d85a88f26f4563936a3395e0372d3af80d2cb3c7
emrecalik <emre.calik01@gmail.com>
2020-04-02 22:33:43 +0300

    adding visit entity. #25

---
+++ pet-clinic-data/src/main/java/com/edoras/petclinic/model/Visit.java
@@ -0,0 +1,35 @@
+package com.edoras.petclinic.model;
+
+import java.time.LocalDate;
+
+public class Visit extends BaseEntity {
+    private LocalDate date;
+
+    private String description;
+
+    private PetType petType;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+}
