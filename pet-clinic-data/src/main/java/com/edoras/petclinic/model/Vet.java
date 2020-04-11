ed589d8013d5e938aa59de31a51a381ab7dd0427
emrecalik <emre.calik01@gmail.com>
2020-04-11 19:12:24 +0300

    converting Vet and Speciality to JPA entities. #36

--- pet-clinic-data/src/main/java/com/edoras/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/edoras/petclinic/model/Vet.java
@@ -1,9 +1,18 @@
 package com.edoras.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "vet_speciality",
+            joinColumns = @JoinColumn(name = "speciality_id"),
+            inverseJoinColumns = @JoinColumn(name = "vet_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
