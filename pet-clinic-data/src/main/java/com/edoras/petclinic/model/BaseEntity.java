2a1a1581d5dbc29f310fda6dd5a52f43b75bab5c
emrecalik <emre.calik01@gmail.com>
2020-04-11 18:35:27 +0300

    implementing JPA base entity. #34

--- pet-clinic-data/src/main/java/com/edoras/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/edoras/petclinic/model/BaseEntity.java
@@ -1,6 +1,16 @@
 package com.edoras.petclinic.model;
 
-public class BaseEntity {
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+
+@MappedSuperclass
+public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
