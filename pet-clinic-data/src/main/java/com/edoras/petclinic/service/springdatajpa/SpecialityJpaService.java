5049b8175817279bd83777f6c72fac5217ba319b
emrecalik <emre.calik01@gmail.com>
2020-04-11 20:34:05 +0300

    creating JPA Service for Speciality. #43

---
+++ pet-clinic-data/src/main/java/com/edoras/petclinic/service/springdatajpa/SpecialityJpaService.java
@@ -0,0 +1,46 @@
+package com.edoras.petclinic.service.springdatajpa;
+
+import com.edoras.petclinic.model.Speciality;
+import com.edoras.petclinic.repository.SpecialityRepository;
+import com.edoras.petclinic.service.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class SpecialityJpaService implements SpecialityService {
+
+    private final SpecialityRepository specialityRepository;
+
+    public SpecialityJpaService(SpecialityRepository specialityRepository) {
+        this.specialityRepository = specialityRepository;
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return specialityRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return specialityRepository.save(object);
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialities = new HashSet<>();
+        specialityRepository.findAll().forEach(specialities::add);
+        return specialities;
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        specialityRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        specialityRepository.deleteById(id);
+    }
+}
