cc5a4eccd9b89a3bd628a806139ed1582dc4ae88
emrecalik <emre.calik01@gmail.com>
2020-04-11 20:27:47 +0300

    creating JPA Service for Pet. #42

---
+++ pet-clinic-data/src/main/java/com/edoras/petclinic/service/springdatajpa/PetJpaService.java
@@ -0,0 +1,46 @@
+package com.edoras.petclinic.service.springdatajpa;
+
+import com.edoras.petclinic.model.Pet;
+import com.edoras.petclinic.repository.PetRepository;
+import com.edoras.petclinic.service.PetService;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class PetJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return petRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petRepository.deleteById(id);
+    }
+}
