7721376291ac58f2e81325198e2d0eceb4f2ca55
emrecalik <62518089+emrecalik@users.noreply.github.com>
2020-05-09 17:36:25 +0300

    implementing JSON endpoint for Vets. #59

--- pet-clinic-web/src/main/java/com/edoras/petclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/edoras/petclinic/controller/VetController.java
@@ -1,9 +1,14 @@
 package com.edoras.petclinic.controller;
 
+import com.edoras.petclinic.model.Vet;
 import com.edoras.petclinic.service.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -17,5 +22,11 @@ public class VetController {
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vet/index";
+    }
+
+    @GetMapping("/api/vets")
+    @ResponseBody
+    public Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
